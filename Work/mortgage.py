5b124bb3c6db13d8053869872736a94c116776db
Noah <bmhskgznhm-commits@apple.privaterelay.com>
2026-08-15 17:56:16 -0700

    Format changes to mortgage.py and created pcost.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -20,7 +20,8 @@ while principal > 0:
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
 
-    print(month, round(total_paid, 2), round(principal, 2))
+    print(
+        f'Month: {month}, Total Paid: ${round(total_paid, 2)}, Principal: ${round(principal, 2)}')
 
 print(f'Total Paid: ${round(total_paid, 2)}')
 print(f'Months: {month}')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,50 @@
 # pcost.py
-#
-# Exercise 1.27
+
+import sys
+import csv
+
+
+def portfolio_cost(filename):
+    '''
+    Computes total cost (shares * price) of a portfolio file
+    '''
+
+    total_cost = 0.0
+
+    with open(filename, 'rt') as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows:
+            try:
+                shares = int(row[1])
+                price = float(row[2])
+                total_cost += shares * price
+            except ValueError:
+                print('Bad row:', row)
+
+    return total_cost
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print(f'Total Cost: ${cost:,.2f}')
+
+
+# Exercise 1_27 'The Original'
+total_cost = 0.0
+
+
+with open('Data/portfolio.csv', 'rt') as file:
+    headers = next(file)
+    for line in file:
+        row = line.split(',')
+        shares = int(row[1])
+        price = float(row[2])
+        total_cost += shares * price
+
+
+print(f'Total Cost: ${total_cost:,.2f}')
